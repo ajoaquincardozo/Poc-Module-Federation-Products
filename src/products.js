41571c729e3afa9c16847de7f7be702ad7e91394
ajoaquincardozo <a.joaquincardozo@gmail.com>
2022-12-19 10:26:33 -0300

    Se agrega jwt, p/ el caso del detalle del producto

--- src/products.js
+++ src/products.js
@@ -1,10 +1,27 @@
-const API_SERVER = "https://poc-module-federation-app-server.vercel.app"//"https://dummyjson.com"
+const API_SERVER = "https://poc-module-federation-app-server.vercel.app" //http://localhost:8081"
+
+const getCookie = (name) => {
+    const cookieByName = document
+        .cookie
+        .split(";")
+        .reduce((result, cookieRaw) => {
+            const [key, value] = cookieRaw.split("=");
+            return { ...result, [key]: value }
+        }, {});
+
+    return cookieByName[name];
+}
 
 export const getProducts = (limit = 11) =>
     fetch(`${API_SERVER}/products?limit=${limit}`).then((res) => res.json());
 
 export const getProductById = (id) =>
-    fetch(`${API_SERVER}/products/${id}`)
+    fetch(`${API_SERVER}/products/${id}`, {
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${getCookie("auth-login")}`,
+        }
+    })
     .then((res) => (res.json()));
 
 export const currency = new Intl.NumberFormat("en-US", {
